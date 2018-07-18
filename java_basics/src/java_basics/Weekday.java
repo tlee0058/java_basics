6fa358848261ef92307754ab84ede18983b68129
Ting Lee <tlee0058@gmail.com>
2018-07-17 21:07:04 -0400

    use of enum

---
+++ java_basics/src/java_basics/Weekday.java
@@ -0,0 +1,23 @@
+package java_basics;
+
+public class Weekday{
+	public enum WeekdayCount{
+	Monday, Tuesday, Wednesday, Thursday, Friday, Saturday, Sunday
+	}
+
+	public static void main(String[] args)
+	{
+		WeekdayCount today = WeekdayCount.Tuesday;
+		
+		if (today == WeekdayCount.Saturday)
+			System.out.println("Weekend, finally!");
+		
+		int i = 1;
+		for (WeekdayCount day : WeekdayCount.values())
+		{
+			System.out.println(i++ + ". day: " + day);
+		}
+	
+}
+}
+
